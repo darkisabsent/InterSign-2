2f32513e27dcba14b0819da729296da28538382e
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-08-26 04:27:14 +0000

    rmv unimplemented

--- frontend/lib/data/dashboard/side_menu_data.dart
+++ frontend/lib/data/dashboard/side_menu_data.dart
@@ -8,9 +8,9 @@ class SideMenuData {
     MenuModel(icon: Icons.translate, title: "Avatar Translation"),
     MenuModel(icon: Icons.multitrack_audio, title: "Translate to Speech"),
     MenuModel(icon: Icons.settings, title: "Settings"),
-    MenuModel(icon: Icons.payment, title: "Payment"),
+    /*MenuModel(icon: Icons.payment, title: "Payment"),
     MenuModel(icon: Icons.account_circle_sharp, title: "Accounts"),
-    MenuModel(icon: Icons.help, title: "Help"),
+    MenuModel(icon: Icons.help, title: "Help"),*/
     //MenuModel(icon: Icons.dashboard_outlined, title: "Dashboard"),
   ];
 }
